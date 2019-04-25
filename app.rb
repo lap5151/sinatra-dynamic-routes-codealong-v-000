c3dd993b16b95f9a645f39bf98d8e1491bdf6e33
Lauren Pardee <lap5151@gmail.com>
2019-04-25 00:36:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,8 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    params[:num1] * params[:num2]
+    @product = params[:num1] * params[:num2]
+    @product.to_i
   end
 
 end
