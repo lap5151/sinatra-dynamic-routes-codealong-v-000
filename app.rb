e5eae1cbad4bd3d09530ed96c6c0922e815e529a
Lauren Pardee <lap5151@gmail.com>
2019-04-25 00:20:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,8 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    params.map do {|key, value| params[key] = value.to_i}
+    #params.map do {|key, value| params[key] = value.to_i}
+    params.map do {|key, value| key: value.to_i}
     params[:num1]
   end
 
